63c3fffa1658ae23a0e28b8bc6e9708335b3eacb
hanhuyeny2k <hanhuyeny2k@gmail.com>
2021-07-06 22:56:30 -0500

    contact info

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,4 +259,28 @@
     }
   });
 
+  /*formspree contact*/
+  var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add('success');
+        status.innerHTML = "Your message has been sent. Thank you!";
+        form.reset()
+      }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
 })()
